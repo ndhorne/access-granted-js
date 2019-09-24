bc3b2bfcf3d567e9cbca9632487bd9f4aa9a25dc
Nicholas D. Horne <55657484+ndhorne@users.noreply.github.com>
2019-09-24 05:32:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# access-granted-js
+Access Granted
+
+A diversion brought to you by Nicholas D. Horne
+
+Crack a PIN knowing the digits that the PIN comprises
+
+Can you actually crack a four-digit PIN on your
+first attempt as seen in the movies on a telltale
+worn keypad? The "worn" keys contained in the
+PIN have been highlighted on the keypad. PINs
+are four digits in length. Digits may be repeated
+resulting in PINs with less than four keys being
+highlighted. PINs may begin with zero.
---
+++ accessgranted.js
@@ -0,0 +1,128 @@
+/*
+Copyright 2018, 2019 Nicholas D. Horne
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+let pin;
+let entry = "";
+let entries = [];
+let buttons = [];
+let lcd = document.getElementById("lcd");
+let resetDisplayTimeout;
+
+for (let i = 0; i < 10; i++) {
+  buttons[i] = document.getElementById("button" + i);
+}
+
+buttons.forEach(button => {
+  button.addEventListener("click", event => {
+    if (entry.length < 4) {
+      entry += event.target.textContent;
+      keyIn();
+    }
+  });
+});
+
+window.addEventListener("keydown", event => {
+  if (/^\d$/.test(event.key)) {
+    if (entry.length < 4) {
+      entry += event.key;
+      keyIn();
+    }
+  }
+});
+
+function about(event) {
+  alert("Access Granted\n" +
+    "\n" +
+    "A Diversion by Nicholas D. Horne\n" +
+    "\n" +
+    "Can you actually crack a four-digit PIN on your\n" +
+    "first attempt as seen in the movies on a telltale\n" +
+    "worn keypad? The \"worn\" keys contained in the\n" +
+    "PIN have been highlighted on the keypad. PINs\n" +
+    "are four digits in length. Digits may be repeated\n" +
+    "resulting in PINs with less than four keys being\n" +
+    "highlighted. PINs may begin with zero.\n");
+  event.preventDefault();
+}
+
+function updateDisplay() {
+  lcd.style.backgroundColor = "darkgrey";
+  lcd.textContent = entry;
+}
+
+function keyIn() {
+  clearTimeout(resetDisplayTimeout);
+  updateDisplay();
+  if (entry.length == 4) {
+    setTimeout(() => verifyEntry(), 500);
+  }
+}
+
+function highlightKeys() {
+  buttons.forEach(button => {
+    button.style.backgroundColor = "";
+  });
+  for (let key of pin) {
+    let button = document.getElementById("button" + key);
+    button.style.backgroundColor = "orange";
+  }
+}
+
+function updateEntries() {
+  entries.push(entry);
+}
+
+function pinGen() {
+  pin = "";
+  for (let i = 0; i < 4; i++) {
+    pin += Math.floor(Math.random() * 10);
+  }
+  //print PIN to console for debugging (or cheating)
+  //console.log(pin);
+  highlightKeys();
+}
+
+function newGame(event) {
+  event.preventDefault();
+  pinGen();
+  entry = "";
+  entries = [];
+  updateDisplay();
+}
+
+function verifyEntry() {
+  if (entry == pin) {
+    lcd.textContent = "Access Granted";
+    lcd.style.backgroundColor = "green";
+    resetDisplayTimeout = setTimeout(() => updateDisplay(), 3000);
+    updateEntries();
+    alert("PIN " + pin + " cracked in " + entries.length +
+      " attempt" + (entries.length > 1 ? "s" : ""));
+    entries = [];
+    pinGen();
+  } else {
+    lcd.textContent = "Access Denied";
+    lcd.style.backgroundColor = "red";
+    resetDisplayTimeout = setTimeout(() => updateDisplay(), 1500);
+    updateEntries();
+  }
+  entry = "";
+}
+
+pinGen();
+//display instructions dialog box upon page load
+//window.addEventListener("load", event => about(event));
